7f433b73bb060e38825ff16566911c0fc4df9196
Tristan Tew <ttew@bu.edu>
2021-05-24 15:56:31 -0400

    Heroku

--- spotipy_webapp/__init__.py
+++ spotipy_webapp/__init__.py
@@ -6,7 +6,7 @@ from .routes import main
 def create_app(config_file='settings.py'):
 
     #Define the application using Flask 
-    app = Flask(__name__)
+    my_app = Flask(__name__)
 
     #Configure the application from the predefined settings.py file 
     app.config.from_pyfile(config_file)
